6ce99048567a2e2db40918389a102ef3c76cb07f
zjjnlz <zjjnlz@users.noreply.github.com>
2018-10-08 08:07:06 +0800

    组件拆分
    进行了代码的整理，过渡动画整合到global文件里；组件进行拆分，分成titlebar和menubar

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,52 +1,38 @@
 <template>
    <div class="ebook">
-       <div class="title-wrapper">
-           <div class="left">
-               <span class="icon-back icon"></span>
-           </div>
-           <div class="right">
-               <div class="icon-wrapper">
-                   <span class="icon-cart icon"></span>
-               </div>
-               <div class="icon-wrapper">
-                   <span class="icon-person icon"></span>
-               </div>
-               <div class="icon-wrapper">
-                   <span class="icon-more icon"></span>
-               </div>
-           </div>
-       </div>
+       <title-bar :ifTitleAndMenuShow = "ifTitleAndMenuShow"></title-bar>
        <div class="read-wrapper">
            <div id="read"></div>
            <div class="mask">
                <div class="left" @click="prevPage"></div>
-               <div class="center"></div>
+               <div class="center" @click="toggleTitleAndMenu"></div>
                <div class="right" @click="nextPage"></div>
            </div>
        </div>
-       <div class="menu-wrapper">
-           <div class="icon-wrapper">
-               <span class="icon-menu icon"></span>
-           </div>
-           <div class="icon-wrapper">
-               <span class="icon-progress icon"></span>
-           </div>
-           <div class="icon-wrapper">
-               <span class="icon-bright icon"></span>
-           </div>
-           <div class="icon-wrapper">
-               <span class="icon-a icon">A</span>
-           </div>
-       </div>
+       <menu-bar :ifTitleAndMenuShow = "ifTitleAndMenuShow"></menu-bar>
    </div>
 </template>
 
 <script>
+import TitleBar from '@/components/TitleBar.vue'
+import MenuBar from '@/components/MenuBar.vue'
 import Epub from 'epubjs'
 global.ePub = Epub
 const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
 export default {
+  components: {
+    TitleBar,
+    MenuBar
+  },
+  data () {
+    return {
+      ifTitleAndMenuShow: false
+    }
+  },
   methods: {
+    toggleTitleAndMenu () {
+      this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+    },
     prevPage () {
       // Rendtition.prev
       if (this.rendition) {
@@ -83,36 +69,6 @@ export default {
 @import 'assets/styles/global';
 .ebook {
     position: relative;
-    .title-wrapper {
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: 101;
-        // 弹性布局
-        display: flex;
-        width: 100%;
-        height: px2rem(48);
-        background: white;
-        // 阴影
-        box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, 0.15);
-        .left {
-            flex: 0 0 px2rem(60);
-            @include center;
-        }
-        .right {
-            flex: 1;
-            display: flex;
-            // 居右排列
-            justify-content: flex-end;
-            .icon-wrapper {
-                flex: 0 0 px2rem(40);
-                @include center;
-                .icon-cart {
-                    font-size: px2rem(22);
-                }
-            }
-        }
-    }
     .read-wrapper {
         .mask {
             position: absolute;
@@ -132,9 +88,6 @@ export default {
                 flex: 0 0 px2rem(100);
             }
         }
-    }
-    .menu-wrapper{
-        position: absolute;
     }
 }
 </style>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="menu-bar">
+        <transition name="slide-up">
+            <div class="menu-wrapper" v-show="ifTitleAndMenuShow">
+                <div class="icon-wrapper">
+                    <span class="icon-menu icon"></span>
+                </div>
+                <div class="icon-wrapper">
+                    <span class="icon-progress icon"></span>
+                </div>
+                <div class="icon-wrapper">
+                    <span class="icon-bright icon"></span>
+                </div>
+                <div class="icon-wrapper">
+                    <span class="icon-a icon">A</span>
+                </div>
+            </div>
+        </transition>
+        <div class="setting-wrapper">
+            <div class="setting-font-seize"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ifTitleAndMenuShow: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/global';
+.menu-bar {
+    .menu-wrapper {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        z-index: 101;
+        // 弹性布局
+        display: flex;
+        width: 100%;
+        height: px2rem(48);
+        background: white;
+        // 阴影
+        box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
+        .icon-wrapper {
+            flex: 1;
+            @include center;
+            .icon-progress {
+                font-size: px2rem(28);
+            }
+            .icon-bright {
+                font-size: px2rem(24);
+            }
+        }
+    }
+    .setting-wrapper {
+        position: absolute;
+        bottom: px2rem(48);
+        z-index: 101;
+        left: 0;
+        width: 100%;
+        height: px2rem(60);
+        background: yellow;
+        box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
+        .setting-font-size {
+
+        }
+    }
+}
+</style>
